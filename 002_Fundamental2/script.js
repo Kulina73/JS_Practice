ea25f3e903e804afbfe795538a118ae08fd9fddf
Kulina73 <KaruKell73@gmail.com>
2021-05-22 21:02:01 -0400

    modified 002 files

--- 002_Fundamental2/script.js
+++ 002_Fundamental2/script.js
@@ -114,7 +114,7 @@ checkWinner(scoreDolphins, scoreKoalas);*/
 
 //Array
 
-const friends = ['Michael', 'Steven', 'Peter'];
+/*const friends = ['Michael', 'Steven', 'Peter'];
 
 const firstName = 'Kulli';
 const kulli = [firstName, 'Maslova', 2037-1973, 'mom', friends];
@@ -149,4 +149,342 @@ console.log(friends.includes(23));
 
 if(friends.includes('Steven')) {
     console.log('You have a friend called Steven');
+}*/
+
+
+//challenge #2
+
+/*const bill = 100;
+
+if (bill >= 50 && bill <= 300) {
+    console.log(`The bill was ${bill}, the tip was ${bill * 0.15} and the total value ${bill - bill * 0.15}`);
+} else {
+    console.log(`The bill was ${bill}, the tip was ${bill * 0.2} and the total value ${bill - bill * 0.2}`);
+} */
+//const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+//console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+
+/*const calcTip = function (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;    
 }
+
+//const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]; 
+console.log(bills, tips, totals);*/
+
+
+/*function calcTip (bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]; 
+console.log(bills, tips, totals);*/
+
+//Objects
+
+/*const kulli = {
+    firstName: 'Kulli',
+    lastName: 'Maslova',
+    age: 2037- 1973,
+    job: 'mom',
+    friends: ['Michael', 'Peter', 'Steven']
+};
+console.log(kulli);
+
+console.log(kulli.lastName);
+console.log(kulli['lastName']);
+
+const nameKey = 'Name';
+console.log(kulli['first' + nameKey]);
+console.log(kulli['last' + nameKey]);
+
+//console.log(kulli.'last' + nameKey)
+
+/*const interestedIn = prompt('What do you want to know about Kulli? Choose between firstName, lastName, age, job, and friends');
+console. log(kulli[interestedIn]);
+
+if (kulli[interestedIn]) {
+    console.log(kulli[interestedIn]);
+} else {
+    console.log('Wrong request! Choose between firstName, lastName, age, job, and friends') 
+}*/
+
+/*kulli.location = 'Atlanta';
+kulli['twitter'] = '@kullimaslova';
+console.log(kulli);*/
+
+//Challenge
+//"Kulli has 3 friends, and her best friend is called Michael"
+
+//console.log(`${kulli.firstName} has ${kulli.friends.length} friends, and her best friend is called ${kulli.friends[0]}`);
+
+//Object methods
+
+/*const kulli = {
+    firstName: 'Kulli',
+    lastName: 'Maslova',
+    birthYear: 1973,
+    job: 'mom',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+    
+    /*calcAge: function (birthYear) {
+        return 2037 - birthYear;
+    } //expression */
+
+    /*calcAge: function () {
+        return 2037 - this.birthYear;
+    } */
+/*
+    calcAge: function () {
+        this.age = 2037 - this.birthYear
+        return this.age;
+    },
+    getSummary: function() {
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and she has ${this.hasDriversLicense ? 'a' : 'no'} driver's licence.`
+    } 
+};
+
+
+//console.log(kulli);
+console.log(kulli.calcAge());
+
+console.log(kulli.age);
+console.log(kulli.age);
+console.log(kulli.age);
+
+
+//console.log(kulli['calcAge'](1973));
+
+//Challenge
+//"Kulli is 64-year old teacher, and she has a driver's license"
+
+/*if (kulli.hasDriversLicense = true) {
+console.log(`${kulli.firstName} is ${kulli.age}-year old ${kulli.job}, and she has a driver's license.`)
+} else {
+    console.log(`${kulli.firstName} is ${kulli.age}-year old ${kulli.job}, but she doesn't have a driver's license.`)
+
+}*/
+
+//console.log(kulli.getSummary());
+
+/*//Challenge #3
+
+const mark = {
+    fullName: 'Mark Miller',
+    weight: 78,
+    height: 1.69,
+    calcBMI: function() {
+        this.bMI = this.weight / this.height **2
+        return this.bMI;
+    }   
+};
+
+const john = {
+    fullName: 'John Smith',
+    weight: 92,
+    height: 1.95,
+    calcBMI: function() {
+        this.bMI = this.weight / this.height **2
+        return this.bMI;
+    }
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+//console.log(mark.bMI, john.bMI);
+
+if (mark.bMI > john.bMI) {
+    console.log(`${mark.fullName}'s BMI (${mark.bMI}) is higher than ${john.fullName}'s BMI (${john.bMI})!`)
+} else {console.log(`${john.fullName}'s BMI (${john.bMI}) is higher than ${mark.fullName}'s BMI (${mark.bMI})!`)
+};*/
+
+//Loop
+
+//for loop keeps running while condition is TRUE
+
+/*console.log('Lifting weights repetition 1');
+console.log('Lifting weights repetition 2');
+console.log('Lifting weights repetition 3');
+console.log('Lifting weights repetition 4');
+console.log('Lifting weights repetition 5');
+console.log('Lifting weights repetition 6');
+console.log('Lifting weights repetition 7');
+console.log('Lifting weights repetition 8');
+console.log('Lifting weights repetition 9');
+console.log('Lifting weights repetition 10');*/
+
+
+//for loop keeps running while condition is TRUE
+
+/*for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}*/
+
+/*const kulli = [
+    'Kulli',
+    'Maslova',
+    2037- 1973,
+    'mom',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+const types = [];
+
+// console.log(kulli[0]);
+// console.log(kulli[1]);
+// ...
+// console.log(kulli[4])
+// kulli[5] does not exist
+
+for (let i = 0; i < kulli.length; i++) {
+    console.log(kulli[i], typeof kulli[i]);
+
+// Filling types array
+
+    //types[i] = typeof kulli[i];
+    types.push(typeof kulli[i]);
+}
+console.log(types);
+
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
+//important!!!
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
+
+//continue and break
+console.log('---ONLY STRINGS---')
+for (let i = 0; i < kulli.length; i++) {
+    if(typeof kulli[i] !== 'string') continue;
+
+    console.log(kulli[i], typeof kulli[i]);
+}
+
+console.log('---BREAK WITH NUMBER---')
+for (let i = 0; i < kulli.length; i++) {
+    if(typeof kulli[i] === 'number') break;
+
+    console.log(kulli[i], typeof kulli[i]);
+}*/
+
+/*const kulli = [
+    'Kulli',
+    'Maslova',
+    2037 - 1973,
+    'mom',
+    ['Michael', 'Peter', 'Steven'],
+    true
+];
+
+//backward loop: 4, 3 , ..., 0
+
+for(let i = kulli.length - 1; i >= 0; i-- ) {
+    console.log(i, kulli[i]);
+}
+
+//loop inside the loop
+
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`-----Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++ ) {
+       console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`); 
+    }
+}*/
+
+//While loop
+
+/*for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+}*/
+
+/*let rep = 1;
+while (rep <= 10) {
+//    console.log(`WHILE:Lifting weights repetition ${rep}`);
+    rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+//console.log(dice);
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`Loop is about to end...`);
+}*/
+
+//Challenge # 4
+
+/*const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(bills[i] >= 50 && bills[i] <= 300 ? bills[i] * 0.15 : bills[i] * 0.2);    
+    totals.push (bills[i] + tips[i]);
+}
+console.log(bills, tips, totals);
+
+/*for (let sum = 0; sum < bills.lenght; sum++) {
+    return 
+    }
+}*/
+
+const calcTip = function (bill) {
+    return bill >=50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+    const tip = calcTip(bills[i]);
+    tips.push(tip);
+    totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+const calcAvarage = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        //sum = sum + arr[i];
+        sum += arr[i];
+    }
+    return sum / arr.length;  
+}
+console.log(calcAvarage([2, 3, 7]));
+console.log(calcAvarage(totals));
+console.log(calcAvarage(tips));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
