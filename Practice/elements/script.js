bf1d30ce9415fa286a92628ffe50a6bf3f750225
Kulina73 <KaruKell73@gmail.com>
2021-06-07 18:53:49 -0400

    modifies 007 files, adds practice , projects folders

---
+++ Practice/elements/script.js
@@ -0,0 +1,9 @@
+const myMain = document.
+
+let my_element = document.createElement("h1");
+let my_span = documeny.createElement("span");
+my_span.innerText = "happy";
+
+my_element.appendChild(my_span);
+
+document.body.appendChild(my_element);
